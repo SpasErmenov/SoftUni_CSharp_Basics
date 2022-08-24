bd83c9aba90195288ad3da400d497f481a56ac05
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-08-24 23:32:13 +0300

    Create Program.cs

---
+++ First Steps In Coding/Exercise/T09. Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            double h = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double percentCalculated = percent / 100;
+            double FishTankvInDm3 = (a * b * h) / 1000;
+            double FishTankWater = FishTankvInDm3 * (1 - percentCalculated);
+
+            Console.WriteLine(FishTankWater);
+
+        }
+    }
+}
