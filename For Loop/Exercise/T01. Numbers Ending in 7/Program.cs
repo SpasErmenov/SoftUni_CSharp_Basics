0d61a6d176b6b9453dff493db24a8956ff6c817f
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-08-28 23:17:11 +0300

    Create Program.cs

---
+++ For Loop/Exercise/T01. Numbers Ending in 7/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace For_loop_upr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
