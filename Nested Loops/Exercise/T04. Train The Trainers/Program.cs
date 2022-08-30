4a0106c323868c16a89e077cbb84a7d13db12344
Spas Ermenov <101284035+SpasErmenov@users.noreply.github.com>
2022-08-30 22:48:54 +0300

    Create Program.cs

---
+++ Nested Loops/Exercise/T04. Train The Trainers/Program.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double totalAverage = 0;
+            double totalNumberOfPresentations = 0;
+            while (true)
+            {
+                string nameOFPresentation = Console.ReadLine();
+
+                if (nameOFPresentation.Equals ("Finish"))
+                {
+                    totalAverage /= (totalNumberOfPresentations * n);
+                    Console.WriteLine($"Student's final assessment is {totalAverage:F2}.");
+                    break;
+                }
+
+                double average = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    double mark = double.Parse(Console.ReadLine());
+                    average += mark;
+                    totalAverage += mark;
+                }
+                average /= n;
+
+                Console.WriteLine($"{nameOFPresentation} - {average:F2}.");
+
+                totalNumberOfPresentations++;
+             }
+        }
+    }
+}
